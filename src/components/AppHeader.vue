0272b5f4f8c47f560650becb3e31f9fdc6c14158
mlietti1 <mcf.lietti@outlook.com>
2022-11-18 19:33:46 +0100

    primo commit

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,12 +5,20 @@ export default {
 </script>
 
 <template>
-  <div class="container">
-    <h1>Breaking Bad</h1>
+  <div class="d-flex p-3">
+    <img src="/Breaking_Bad_logo.svg" alt="logo">
+    <h1>Breaking Bad Api</h1>
   </div>
 </template>
 
 <style lang="scss" scoped>
 
+img {
+  height: 40px;
+  margin-right: 5px;
+}
+h1 {
+  font-weight: 600;
+}
 
 </style>
